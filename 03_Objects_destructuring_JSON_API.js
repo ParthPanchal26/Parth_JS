d329ef44314bddf4661f1f030b9caa366671e8cd
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-02-02 22:56:34 +0530

    Objects destructuring

---
+++ 03_Objects_destructuring_JSON_API.js
@@ -0,0 +1,9 @@
+const course = {
+    name: "BackEnd In Js",
+    price: 3999,
+    courseInstructor: "Hitesh sir"
+};
+
+const { courseInstructor: instructor } = course;
+
+console.log(instructor);
