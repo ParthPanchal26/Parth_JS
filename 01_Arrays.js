a92c4aa5fbfba23ba79353fe27cbe2bb9ead8ba2
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-02-02 19:58:23 +0530

    Few more advanced array methods

--- 01_Arrays.js
+++ 01_Arrays.js
@@ -26,3 +26,42 @@ console.log("\nAfter slice method : ", arr);
 console.log("\nBefore splice method : ", arr);
 console.log("\nSpliced array : ", arr.splice(1, 3));
 console.log("\nAfter splice method : ", arr);
+
+
+const marvel_heros = ["SpiderMan", "IronMan", "Thor"];
+const DC_heros = ["SuperMan", "BatMan", "Flash"];
+
+console.log("\nMarvel Heros are : ", marvel_heros);
+console.log("\nDC Heros are : ", DC_heros);
+
+marvel_heros.push(DC_heros);
+
+console.log("\nMearged array of marvel_heros and DC_heros is : ", marvel_heros);
+
+console.log("\nAfter PUSH of DC_heros, hero at index[3][1] : ", marvel_heros[3][1]);
+
+/* Using push method to mearge two array is bit inconvenient so we use spread operator (...) to mearge two arrays */
+
+const marvel = ["SpiderMan", "IronMan", "Thor"];
+const DC = ["SuperMan", "BatMan", "Flash"];
+const all_heros = [...marvel, ...DC];
+console.log("\nAfter using spread operator : ", all_heros);
+
+
+const complicated_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+console.log("\nComplicated array is : ", complicated_array);
+
+const easy_solved_array = complicated_array.flat(Infinity);
+console.log("\Complicated array into single easy array using flat method is : ", easy_solved_array);
+
+/* Array.from(argument) used to implement array from given argument */
+console.log("\nUsing Array.from method on string \"Parth\" : ", Array.from("Parth"));
+
+/* Array.isArray(argument) used to ask is given argument is array or not! */
+console.log("\nIs \"Parth\" array : ", Array.isArray("Parth"));
+
+/* Array.of(argument) is used to create an array from given arguments */
+let arr_val_1 = "Parth";
+let arr_val_2 = "Dipakbhai";
+let arr_val_3 = "Panchal";
+console.log("\nArray using variables : ", Array.of(arr_val_1, arr_val_2, arr_val_3));
