5c7acba7e0c1ed1e8d8f4756bb0fb341f2002a5a
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-02-06 08:27:04 +0530

    BackGround color changer

---
+++ 11_colors.js
@@ -0,0 +1,26 @@
+const randomColor = () => {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+let changeBG;
+
+const startChangingColor = function () {
+    const changeBGcolor = () => {
+        document.body.style.backgroundColor = randomColor();
+    }
+    if (!changeBG) {
+        changeBG = setInterval(changeBGcolor, 600);
+    }
+};
+
+const stopChangingColor = function () {
+    clearInterval(changeBG);
+    changeBG = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
